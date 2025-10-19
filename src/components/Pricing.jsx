1292ad7002f3184ca749ccebf21e12388e9f74a7
Piyush Yadav <158178653+piyushrajyadav@users.noreply.github.com>
2025-10-19 19:02:52 +0530

    Refactor Pricing component layout and image alt text

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import PricingPage from './PricingPage'
 
 function Pricing() {
   return (
-    <div className='w-full overflow-x-hidden bg-white'>
-      <img 
-        src='pricing.png' 
-        alt='pricingImg' 
-        className='w-full h-auto max-w-full object-contain'
+    <div className='w-full bg-white overflow-x-hidden'>
+      {/* Responsive Pricing Banner */}
+      <img
+        src='pricing.png'
+        alt='Pricing Banner'
+        className='w-full h-auto object-contain md:object-cover'
       />
+
+      {/* Pricing Section */}
+      <div className='px-4 sm:px-8 md:px-12 lg:px-20 py-8 md:py-16'>
+        <PricingPage />
+      </div>
     </div>
   )
 }
